d9ebff59a18530b24e13b429de543a31a5454ae6
kedmenecr <kedmenec.roberto@gmail.com>
2018-11-07 13:25:19 +0100

    Add with randomUser.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css'
 export default () => {
   const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    document.title = ` You clicked ${count} times`
-  })
+  const [person, setPerson] = useState(null)
+
+  // similar compDidMount and compDidUpdate
+  useEffect(async () => {
+    const response = await fetch('https://api.randomuser.me/')
+    const data = await response.json()
+    const [item] = data.results
+    setPerson(item)
+  }, [count])
 
   return (
     <div>
@@ -14,6 +20,8 @@ export default () => {
         You cliked {count} times
       </p>
       <button onClick={() => setCount(count + 1)}>+</button>
+
+      {person && <div>{person.name.first}</div>}
     </div>
   )
 }
